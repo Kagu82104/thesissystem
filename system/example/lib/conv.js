91d547693aa6215339a14d6a677122152b5e2ed6
Kagu82104 <vincent82104@gmail.com>
2019-07-24 21:20:39 +0800

    me

--- system/example/lib/conv.js
+++ system/example/lib/conv.js
@@ -297,7 +297,9 @@ $(function () {
 	$("#yolo_table tr td").click(function () {
 		var title_value = $.trim($(this).parents().eq(0).find("td:eq(0)").text());
 		var title_name = $.trim($(this).parents().eq(0).find("td:eq(1)").text());
-		var pic_times = $.trim($(this).parents().eq(0).find("td:eq(2)").text());
+        var pic_times = $.trim($(this).parents().eq(0).find("td:eq(2)").text());
+        if(title_value=="28"){title_value=29;}
+        else if(title_value=="29"){title_value=30;}
 		if ($.trim($(this).index()) == 4) {
 			if (title_name === "Convolutional") {
 				$("#placehere").empty();
@@ -331,9 +333,63 @@ $(function () {
 				$("#placehere").empty();
 				var title_h3 = document.createElement("h3");
 				title_h3.className = "center";
-				var t = document.createTextNode("第" + title_value + "層池化結果");
+                var t = document.createTextNode("第" + title_value + "層池化結果");
+                
+                console.log($.trim($(this).parent().prev().find("td:eq(2)").text()));
+                var pic_times = $.trim($(this).parent().prev().find("td:eq(2)").text());
 				title_h3.appendChild(t);
-				document.getElementById('placehere').append(title_h3);
+                document.getElementById('placehere').append(title_h3);
+                for (i = 0; i < pic_times; i++) {
+					var elem = document.createElement("img");
+					var div = document.createElement("div");
+					div.id = "divcon" + title_value + "_" + i;
+					div.className = "ImgInline"
+					elem.setAttribute("src", "data/img/maxpool" + title_value + "_" + i + ".jpg");
+					elem.className = "col-xs-6 rounded img-fluid";
+					elem.id = "img" + title_value + "_" + i;
+					var img_size = 100;
+					if (pic_times == 128) { img_size /= 1.5; }
+					else if (pic_times == 256) { img_size /= 2; }
+					else if (pic_times == 256 || pic_times == 425) { img_size /= 2.5; }
+					else if (pic_times == 512) { img_size /= 3; }
+					else if (pic_times == 1024) { img_size /= 4; }
+					elem.setAttribute("height", img_size);
+					elem.setAttribute("width", img_size);
+
+					document.getElementById('placehere').appendChild(div);
+					document.getElementById("divcon" + title_value + "_" + i).appendChild(elem);
+				}
+            }
+            else if (title_name == "Reorg") {
+                $("#placehere").empty();
+                var title_value = 27;
+				var title_h3 = document.createElement("h3");
+				title_h3.className = "center";
+                var t = document.createTextNode("第" + title_value + "層重組結果");
+                
+                var pic_times = 256;
+				title_h3.appendChild(t);
+                document.getElementById('placehere').append(title_h3);
+                for (i = 0; i < pic_times; i++) {
+					var elem = document.createElement("img");
+					var div = document.createElement("div");
+					div.id = "divcon" + title_value + "_" + i;
+					div.className = "ImgInline"
+					elem.setAttribute("src", "data/img/conv" + title_value + "_" + i + ".jpg");
+					elem.className = "col-xs-6 rounded img-fluid";
+					elem.id = "img" + title_value + "_" + i;
+					var img_size = 100;
+					if (pic_times == 128) { img_size /= 1.5; }
+					else if (pic_times == 256) { img_size /= 2; }
+					else if (pic_times == 256 || pic_times == 425) { img_size /= 2.5; }
+					else if (pic_times == 512) { img_size /= 3; }
+					else if (pic_times == 1024) { img_size /= 4; }
+					elem.setAttribute("height", img_size);
+					elem.setAttribute("width", img_size);
+
+					document.getElementById('placehere').appendChild(div);
+					document.getElementById("divcon" + title_value + "_" + i).appendChild(elem);
+				}
 			}
 		}
 		else if ($.trim($(this).index()) == 3) {
